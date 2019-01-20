adda30f48c7e2f602ac239f39b30751efe74a04a
romil.shah <romil.shah@pharmeasy.in>
2019-01-20 19:23:55 +0530

    Fetch API data

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "738551b37f8d1de407a5";
+/******/ 	var hotCurrentHash = "658aeef5dd150996e9ff";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
